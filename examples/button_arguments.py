c6f753bf76ac429f4d417f92dd4202b544f6a397
Austin Cory Bart <acbart@vt.edu>
2024-05-05 11:17:33 -0400

    Update example

--- examples/button_arguments.py
+++ examples/button_arguments.py
@@ -33,19 +33,14 @@ def buy_page(state: State, apples: int, oranges: int, pears: int, plums: str, fr
 
 assert_equal(
     index(State()),
-    Page(state=State(),
-         content=['Welcome to my site!',
-                  TextBox(name='pears', kind='text', default_value=7),
-                  TextBox(name='plums', kind='text', default_value='3'),
-                  Argument(name='apples', value=5),
-                  Button(text='Buy', url='/buy_page', arguments=Argument(name='oranges', value=7))]))
+    Page(state=State(), content=['Welcome to my site!', TextBox(name='pears', kind='text', default_value=7), TextBox(name='plums', kind='text', default_value='3'), Argument(name='apples', value=5), Argument(name='words', value='ups and downs'), Argument(name='check', value=True), Button(text='Buy', url='/buy_page', arguments=[Argument(name='oranges', value=7), Argument(name='fruits', value='oranges and pears and more'), Argument(name='bonus', value=False)])]))
 
 assert_equal(
     buy_page(State(), 5, 7, 7, '3', 'oranges and pears and more', 'test and test', True, False),
-    Page(state=State(), content=['You bought 5 apples, 7 oranges, 3 plums, and 7 pears']))
+    Page(state=State(), content=['You bought 5 apples, 7 oranges, 3 plums, and 7 pears. (oranges and pears and more) (test and test) (True) (False)']))
 
 assert_equal(
     buy_page(State(), 5, 7, 100, '200', 'oranges and pears and more', 'test and test', False, True),
-    Page(state=State(), content=['You bought 5 apples, 7 oranges, 200 plums, and 100 pears']))
+    Page(state=State(), content=['You bought 5 apples, 7 oranges, 200 plums, and 100 pears. (oranges and pears and more) (test and test) (False) (True)']))
 
 start_server(State())
