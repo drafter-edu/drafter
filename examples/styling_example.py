8f5186e8de68889a56d9df0ea3b33df0b761cced
Austin Cory Bart <acbart@vt.edu>
2024-11-12 09:35:29 -0500

    More styling examples and documentation

---
+++ examples/styling_example.py
@@ -0,0 +1,31 @@
+from drafter import *
+
+set_website_style("none")
+add_website_css("""
+body {
+    background-color: lightblue;
+    font-size: 20px;
+}
+
+.name-box {
+    background-color: pink;
+    float: right;
+}
+""")
+
+@route
+def index(state: str) -> Page:
+    return Page(state, [
+        """<style>
+        /* Page specific style */
+        .name-box {
+            margin: 10px;
+        }
+        </style>
+        """,
+        bold("Welcome to the website!"),
+        TextBox("Name", "Your name goes here", classes="name-box"),
+        Button("Quit", index, style_color="red", style_float='right')
+    ])
+
+start_server("")
