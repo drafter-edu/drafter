d09cac31ad87c9406ec50278c88fb57a3ca6bf9c
Austin Cory Bart <acbart@vt.edu>
2026-07-22 21:52:59 -0400

    Dead code elimination

--- src/drafter/constants.py
+++ src/drafter/constants.py
@@ -1,7 +1,6 @@
 """
 Constants that are used throughout the project.
 """
-RESTORABLE_STATE_KEY = "--restorable-state"
-SUBMIT_BUTTON_KEY = '--submit-button'
 
-JSON_DECODE_SYMBOL = "$@JSON~@$"
+RESTORABLE_STATE_KEY = "--restorable-state"
+SUBMIT_BUTTON_KEY = "--submit-button"
